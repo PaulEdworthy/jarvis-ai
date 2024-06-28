d55396ecd999b2b9c3242886cc6dd591c0f1f96f
PaulEdworthy <pauledworthy@gmail.com>
2024-06-28 11:12:00 -0400

    add updated info and checked for response succes

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
+# import pathlib
+# import textwrap
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv('API_KEY')
-api_secret = os.getenv('API_SECRET')
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+genai.configure(api_key=gemini_api_key)
+# api_secret = os.getenv('API_SECRET')
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+response = model.generate_content("When was Google founded?")
+
+print(response.text)
